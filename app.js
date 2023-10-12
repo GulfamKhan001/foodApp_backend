cf188a5fcd87492431b2c59f2d8391f4a92e8bfb
GulfamKhan001 <Gulfamkajla@gmail.com>
2023-10-12 18:16:50 +0530

    :wheelchair: Public drc removed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 let cors = require("cors");
-
+console.log(process.env.HELLO);
 app.use(cors());
 app.use(express.static('public/build'));
 const cookieParser = require("cookie-parser");
@@ -14,7 +14,7 @@ const authRouter = require('./Routers/authRouter');
 const planRouter = require("./Routers/PlanRouter");
 const reviewRouter = require("./Routers/reviewRouter");
 // const bookingRouter = require("./Routers/bookingRouter");
-console.log(process.env.HELLO);
+
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
 app.use("/plans", planRouter);
